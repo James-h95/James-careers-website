23ef7d4960cf5ca16f3946b541abacf793048ba0
James-h95 <101450428+James-h95@users.noreply.github.com>
2023-12-30 02:22:11 +0000

    Saved application to DB

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify, request
-from database import load_jobs_from_db, load_job_from_db
+from database import add_application_to_db, load_jobs_from_db, load_job_from_db
 
 
 app = Flask(__name__)
@@ -27,6 +27,7 @@ def show_job(id):
 def apply_job(id):
   data = request.form
   job = load_job_from_db(id)
+  add_application_to_db(id, data)
   #store this in the DB
   # Send email to applicant
   # Display an acknowledgement
--- database.py
+++ database.py
@@ -33,6 +33,57 @@ def load_job_from_db(id):
     else:
       return dict(rows[0])
 
+'''
+def add_application_to_db(job_id, data):
+  with engine.connect() as conn:
+    query = text("INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url ) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)")
+
+  conn.execute(query,
+               job_id=job_id ,
+               full_name=data['full_name'], 
+               email=data['email'], 
+               linkedin_url=data['linkedin_url'], 
+               education=data['education'], 
+               work_experience=data['work_experience'], 
+               resume_url=data['resume_url'])
+'''
+
+def add_application_to_db(job_id, data):
+  with engine.connect() as conn:
+      query = text("""
+          INSERT INTO applications (
+              job_id, 
+              full_name, 
+              email, 
+              linkedin_url, 
+              education, 
+              work_experience, 
+              resume_url
+          ) VALUES (
+              :job_id, 
+              :full_name, 
+              :email, 
+              :linkedin_url, 
+              :education, 
+              :work_experience, 
+              :resume_url
+          )
+      """)
+
+      # Prepare a dictionary of parameters
+      params = {
+          'job_id': job_id,
+          'full_name': data.get('full_name'),
+          'email': data.get('email'),
+          'linkedin_url': data.get('linkedin_url'),
+          'education': data.get('education'),
+          'work_experience': data.get('work_experience'),
+          'resume_url': data.get('resume_url')
+      }
+
+      # Execute the query with the parameters
+      conn.execute(query, params)
+
 
 
 
